fb214b436ce060ed87644f9e94d0c51b6ea20f74
David Fernandez <i.am.david.fernandez@gmail.com>
2017-05-29 19:15:50 +1000

    <DF> Made most things (features, colours, etc.) configurable.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,9 +1,36 @@
 function __iadf_init_prompt
+
+    ## Left-side features
+    set -q theme_iadf_feature_context; or set -g theme_iadf_feature_context ""
+    set -q theme_iadf_feature_docker; or set -g theme_iadf_feature_docker ""
+    set -q theme_iadf_feature_userhost; or set -g theme_iadf_feature_userhost ""
+    set -q theme_iadf_feature_path; or set -g theme_iadf_feature_path ""
+    set -q theme_iadf_feature_vcs; or set -g theme_iadf_feature_vcs ""
+    ## Left-side colours
+    set -q theme_iadf_segment_colour_context; or set -g theme_iadf_segment_colour_context "green"
+    set -q theme_iadf_text_colour_context; or set -g theme_iadf_text_colour_context "brblack"
+    set -q theme_iadf_segment_colour_userhost; or set -g theme_iadf_segment_colour_userhost "black"
+    set -q theme_iadf_text_colour_userhost; or set -g theme_iadf_text_colour_userhost "brblack"
+    set -q theme_iadf_segment_colour_path; or set -g theme_iadf_segment_colour_path "blue"
+    set -q theme_iadf_text_colour_path; or set -g theme_iadf_text_colour_path "white"
+    set -q theme_iadf_segment_colour_docker; or set -g theme_iadf_segment_colour_docker "cyan"
+    set -q theme_iadf_text_colour_docker; or set -g theme_iadf_text_colour_docker "black"
+    set -q theme_iadf_segment_colour_vcs; or set -g theme_iadf_segment_colour_vcs "black"
+
+    ## Right-side features
+    set -q theme_iadf_feature_status; or set -g theme_iadf_feature_status "enable"
+    set -q theme_iadf_feature_datetime; or set -g theme_iadf_feature_datetime "enable"
+    ## Right-side colours
+    set -q theme_iadf_segment_colour_datetime; or set -g theme_iadf_segment_colour_datetime "blue"
+    set -q theme_iadf_text_colour_datetime; or set -g theme_iadf_text_colour_datetime "white"
+
+
+    set -q theme_iadf_format_datetime; or set -g theme_iadf_format_datetime "%H:%M:%S"
     set -q theme_iadf_context; or set -g theme_iadf_context ""
 
-    set -q theme_iadf_vcs_list; or set -g theme_iadf_vcs_list git svn hg
-
-    set -q theme_iadf_colour_vcs; or set -g theme_iadf_colour_vcs 000
+    ## VCS feature configuration
+    #set -q theme_iadf_vcs_list; or set -g theme_iadf_vcs_list git svn hg
+    set -q theme_iadf_vcs_list; or set -g theme_iadf_vcs_list git svn
 
     set -q theme_iadf_vcs_colour_dirty; or set -g theme_iadf_vcs_colour_dirty "red"
     set -q theme_iadf_vcs_colour_clean; or set -g theme_iadf_vcs_colour_clean "green"
@@ -19,19 +46,76 @@ function __iadf_init_prompt
     set -q theme_iadf_vcs_glyph_modified; or set -g theme_iadf_vcs_glyph_modified "±"
     set -q theme_iadf_vcs_colour_modified; or set -g theme_iadf_vcs_colour_modified "green"
 
-    set -e __iadf_segment_colour
+    set -g theme_iadf_segment_left_glyph \uE0B0
+    set -g theme_iadf_segment_right_glyph \uE0B2
 
-    set -g __theme_iadf_segment_left_glyph \uE0B0
-    set -g __theme_iadf_segment_right_glyph \uE0B2
-
-    set -g __theme_iadf_left_glyph \uE0B1
-    set -g __theme_iadf_right_glyph \uE0B3
+    set -g theme_iadf_left_glyph \uE0B1
+    set -g theme_iadf_right_glyph \uE0B3
 
     set_color normal
+
+    set -e __iadf_segment_colour
+    set -g __theme_iadf_prompt_left ""
+end
+
+function __iadf_append_prompt
+    #set __theme_iadf_prompt_left "$__theme_iadf_prompt_left"(echo $argv)
+    echo $argv
+end
+
+# function __iadf_show_segment_boundary_left -a colour_left -a colour_right
+#     set_color -b $colour_right $colour_left
+#     echo -n \uE0B0
+#     set_color normal
+# end
+
+function __iadf_start_segment_left -a colour
+    set_color normal
+    if set -q __iadf_segment_colour
+        set_color -b $colour $__iadf_segment_colour
+        echo -n $theme_iadf_segment_left_glyph
+    end
+
+    set -g __iadf_segment_colour $colour
+    set_color -b $colour
 end
 
 
-function __iadf_vcs_reset
+function __iadf_show_prompt_context
+    if test -n $theme_iadf_context
+        __iadf_start_segment_left $theme_iadf_segment_colour_context
+        set_color $theme_iadf_text_colour_context
+        echo -n $theme_iadf_context
+    end
+end
+
+
+function __iadf_show_prompt_for_docker
+    if test -e "/.dockerenv"
+        __iadf_start_segment_left $theme_iadf_segment_colour_docker
+        set_color $theme_iadf_text_colour_docker
+        echo -n "DOCKED "
+    end
+end
+
+
+function __iadf_show_prompt_for_user_host
+    set -l hostname (hostname)
+    __iadf_start_segment_left $theme_iadf_segment_colour_userhost
+    set_color $theme_iadf_text_colour_userhost
+    echo -ns (whoami) '@' $hostname ' '
+end
+
+
+function __iadf_show_prompt_for_path
+    set path (prompt_pwd)
+    __iadf_start_segment_left $theme_iadf_segment_colour_path
+    set_color $theme_iadf_text_colour_path
+    echo -n "$path"
+end
+
+
+function __iadf_vcs_reset -d "Reset vcs state variables"
     set -g __vcs_present 0
     set -g __vcs_branch ""
     set -g __vcs_dirty 0
@@ -40,7 +124,8 @@ function __iadf_vcs_reset
     set -g __vcs_modified 0
 end
 
-function __iadf_vcs_refresh_git
+
+function __iadf_vcs_refresh_git -d "Set vcs state variables for git in the current directory."
 
     __iadf_vcs_reset
 
@@ -78,7 +163,8 @@ function __iadf_vcs_refresh_git
     end
 end
 
-function __iadf_vcs_refresh_hg
+
+function __iadf_vcs_refresh_hg -d "Set vcs state variables for mercurial in the current directory."
 
     __iadf_vcs_reset
 
@@ -110,7 +196,8 @@ function __iadf_vcs_refresh_hg
     end
 end
 
-function __iadf_vcs_refresh_svn
+
+function __iadf_vcs_refresh_svn -d "Set vcs state variables for subversion in the current directory."
 
     __iadf_vcs_reset
 
@@ -148,9 +235,10 @@ function __iadf_vcs_refresh_svn
     end
 end
 
-function __iadf_show_prompt_for_vcs --argument vcs_type
 
-    __iadf_start_segment_left $theme_iadf_colour_vcs
+function __iadf_show_prompt_for_vcs_type --argument vcs_type
+
+    __iadf_start_segment_left $theme_iadf_segment_colour_vcs
 
     set_color brblack
     echo -n "["
@@ -183,40 +271,14 @@ function __iadf_show_prompt_for_vcs --argument vcs_type
     echo -n "]"
 end
 
-# function __iadf_show_segment_boundary_left -a colour_left -a colour_right
-#     set_color -b $colour_right $colour_left
-#     echo -n \uE0B0
-#     set_color normal
-# end
 
-function __iadf_start_segment_left -a colour
-    set_color normal
-    if set -q __iadf_segment_colour
-        set_color -b $colour $__iadf_segment_colour
-        echo -n $__theme_iadf_segment_left_glyph
+function __iadf_show_prompt_for_vcs
+    for vcs in $theme_iadf_vcs_list
+        eval __iadf_vcs_refresh_$vcs
+        if test $__vcs_present -ne 0
+            __iadf_show_prompt_for_vcs_type $vcs
+        end
     end
-
-    set -g __iadf_segment_colour $colour
-    set_color -b $colour
-end
-
-function __iadf_show_prompt_for_path
-    set path (prompt_pwd)
-    __iadf_start_segment_left blue
-    echo -n "$path"
-end
-
-function __iadf_show_prompt_for_docker
-    if test -e "/.dockerenv"
-        __iadf_start_segment_left cyan
-        echo -n "DOCKED "
-    end
-end
-
-function __iadf_show_prompt_for_user_host
-    set -l hostname (hostname)
-    set_color brblack
-    echo -ns (whoami) '@' $hostname ' '
 end
 
 
@@ -225,27 +287,33 @@ function fish_prompt
 
     __iadf_init_prompt
 
-    __iadf_show_prompt_for_docker
-
-    __iadf_show_prompt_for_user_host
-
-    if test -n $theme_iadf_context
-        set_color blue
-        echo -n "[$theme_iadf_context] "
+    if test -n $theme_iadf_feature_context
+        __iadf_show_prompt_context
     end
 
-    __iadf_show_prompt_for_path
+    if test -n $theme_iadf_feature_docker
+        __iadf_show_prompt_for_docker
+    end
 
-    for vcs in $theme_iadf_vcs_list
-        eval __iadf_vcs_refresh_$vcs
-        if test $__vcs_present -ne 0
-            __iadf_show_prompt_for_vcs $vcs
-        end
+    if test -n $theme_iadf_feature_userhost
+        __iadf_show_prompt_for_user_host
+    end
+
+    if test -n $theme_iadf_feature_path
+        __iadf_show_prompt_for_path
+    end
+
+    if test -n $theme_iadf_feature_vcs
+        __iadf_show_prompt_for_vcs
     end
 
     __iadf_start_segment_left normal
 
+    if test (string length $__theme_iadf_prompt_left) -gt 180
+        echo ""
+    end
+
     set_color normal
     set_color yellow
-    echo "$__theme_iadf_left_glyph "
+    echo "$theme_iadf_left_glyph "
 end
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,14 +1,8 @@
-# function __iadf_show_segment_boundary_right -a colour_left -a colour_right
-#     set_color -b $colour_left $colour_right
-#     echo -n \uE0B2
-#     set_color normal
-# end
-
 function __iadf_start_segment_right -a colour
     if set -q __iadf_segment_colour
         set_color normal
         set_color -b $__iadf_segment_colour $colour
-        echo -n $__theme_iadf_segment_right_glyph
+        echo -n $theme_iadf_segment_right_glyph
     end
 
     set -g __iadf_segment_colour $colour
@@ -16,9 +10,23 @@ function __iadf_start_segment_right -a colour
 end
 
 
-function fish_right_prompt
-    # Customize the right prompt
+function __iadf_show_prompt_status -a real_status
+    if test $real_status -ne 0
+        __iadf_start_segment_right red
+        set_color black
+        echo -n "$real_status"
+    end
+end
 
+
+function __iadf_show_prompt_datetime
+    __iadf_start_segment_right $theme_iadf_segment_colour_datetime
+    set_color $theme_iadf_text_colour_datetime
+    date +"$theme_iadf_format_datetime"
+end
+
+
+function fish_right_prompt
     ## Store _actual_ current status for later use
     set -l real_status $status
 
@@ -26,19 +34,17 @@ function fish_right_prompt
 
     set_color normal
     set_color yellow
-    echo "$__theme_iadf_right_glyph "
+    echo "$theme_iadf_right_glyph"
 
     __iadf_start_segment_right 000
 
-    if test $real_status -ne 0
-        __iadf_start_segment_right red
-        set_color black
-        echo -n "$real_status"
+    if test -n $theme_iadf_feature_status
+        __iadf_show_prompt_status $real_status
     end
 
-    __iadf_start_segment_right blue
-    set_color white
-    date +"%H:%M:%S"
+    if test -n $theme_iadf_feature_datetime
+        __iadf_show_prompt_datetime
+    end
 
     set_color normal
 end
